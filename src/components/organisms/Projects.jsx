79c1cddd1e6cbe46a735ccbe0ec68bf5e094c94a
Jelmerovereem <jelmer_overeem@hotmail.com>
2021-08-10 12:56:28 +0200

    added gyro project and added tech used

--- src/components/organisms/Projects.jsx
+++ src/components/organisms/Projects.jsx
@@ -4,6 +4,7 @@ import * as colors from 'styles/colors';
 import PrimaryBtn from 'components/atoms/PrimaryBtn';
 import Fade from 'react-reveal/Fade';
 import LechaimImg from 'assets/img/projects/lechaim.jpg';
+import GyroImg from 'assets/img/projects/gyro.jpg';
 import WeatherAppImg from 'assets/img/projects/progressive_weather_app.png';
 import AudioBookImg from 'assets/img/projects/audioBook.jpg';
 import TrackyjsImg from 'assets/img/projects/tracking_tool_trackyjs.png';
@@ -71,10 +72,21 @@ const ProjectDesc = styled.p`
 	color: ${colors.textColor};
 	opacity: .6;
 	margin-top: 0;
-	margin-bottom: 50px;
+	margin-bottom: 30px;
 	@media screen and (max-width:  768px) {
 		font-size: 1.5em;
 	}
+`
+
+const Tech = styled.span`
+	color: white;
+	background: #b9b9b9;
+	padding: 5px 10px;
+	border-radius: 10px;
+	margin-bottom: 10px;
+	margin-right: 10px;
+	display: block;
+	width: fit-content;
 `
 
 const ProjectImg = styled.img`
@@ -95,25 +107,36 @@ const Projects = () => {
 		title: "Lechaim",
 		description: "Lechaim Official is a clothing company I started with a friend. I've built the whole webshop on my own. It has a admin dashboard, payment options(through Stripe), shipment integration, and other cool features that come with a webshop.",
 		img: LechaimImg,
-		link: "https://www.lechaimofficial.com/"
+		link: "https://www.lechaimofficial.com/",
+		tech: ['Node.js', 'MongoDB', 'Stripe', 'Sendcloud API', 'GA', 'FB Pixel']
+	},
+	{
+		title: "Gyro",
+		description: "Gyro is a web app for therapists to train their patients with mobility issues. The app simulates controlling an experimental gyroscope-backpack in development by TU Delft & Erasmus MC.",
+		img: GyroImg,
+		link: "https://gyro-project.herokuapp.com/",
+		tech: ['Node.js', 'Vue']
 	},
 	{
 		title: "Progressive Weather app",
 		description: "I've built an weather app based on a interactive map. The special thing about this app, is that it's a progressive webapp. It has: static building, Manifest file, Service worker for caching files and many more.",
 		img: WeatherAppImg,
-		link: "https://progressive-weatherapp.herokuapp.com/"
+		link: "https://progressive-weatherapp.herokuapp.com/",
+		tech: ['Node.js', 'Gulp', 'Leaflet', 'OpenWeather API']
 	},
 	{
 		title: "AudioBook",
 		description: "AudioBook is a website made for a man with vision disorder(macular degeneration). This project involves 4 weeks of concepting, testing & prototyping. At the end of the project I've made an website where the navigation is based on speech, key presses or mouse clicks. The website responds with colors and my own voice(I've recorded everything myself!).",
 		img: AudioBookImg,
-		link: "https://jelmerovereem.github.io/human-centered-design-2021/prototype/index.html"
+		link: "https://jelmerovereem.github.io/human-centered-design-2021/prototype/index.html",
+		tech: ['SpeechRecognition API']
 	},
 	{
 		title: "Real time tracking tool - trackyjs",
 		description: "Trackyjs is a tracking tool / analytics tool to track visitors realtime. It's a small project to play and understand working with realtime sockets.",
 		img: TrackyjsImg,
-		link: "https://trackyjs.herokuapp.com/admin"
+		link: "https://trackyjs.herokuapp.com/admin",
+		tech: ['Node.js', 'socketIO', 'geoip']
 	}
 	]
 	return (
@@ -127,6 +150,13 @@ const Projects = () => {
 							<div>
 								<ProjectTitle>{project.title}</ProjectTitle>
 								<ProjectDesc>{project.description}</ProjectDesc>
+								<div style={{display: "flex", flexWrap: "wrap", marginBottom: "30px"}}>
+								{
+									project.tech.map((skill, index) => {
+										return <Tech key={index}>{skill}</Tech>
+									})
+								}
+								</div>				
 								<PrimaryBtn text="Visit Website" href={project.link} newTab={true} small={true} />
 							</div>
 							<div>
