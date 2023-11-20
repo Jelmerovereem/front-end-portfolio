dd34429cd7437aafff895a9c64d1760d0d11b75f
Jelmerovereem <jelmer_overeem@hotmail.com>
2023-11-20 22:49:19 +0100

    update configuration, format and starting on cookiebar

---
+++ src/components/molecules/CookieBar.jsx
@@ -0,0 +1,16 @@
+// @ts-check
+
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  position: fixed;
+  bottom: 20px;
+  background-color: white;
+`;
+
+const CookieBar = () => {
+  return <Wrapper>Accepteer cookies</Wrapper>;
+};
+
+export default CookieBar;
