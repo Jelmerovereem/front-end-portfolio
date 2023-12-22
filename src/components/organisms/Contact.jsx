00b0632fde15da71a4c9708aa796e4420696d33d
Jelmerovereem <jelmer_overeem@hotmail.com>
2023-12-22 20:44:10 +0100

    fix button in/on contact Fixes #12

--- src/components/organisms/Contact.jsx
+++ src/components/organisms/Contact.jsx
@@ -1,156 +1,179 @@
-import React, { useState } from 'react';
-import styled, { css } from 'styled-components';
-import * as colors from 'styles/colors';
-import * as defaults from 'styles/defaults';
+import React, { useState } from "react";
+import styled, { css } from "styled-components";
+import * as colors from "styles/colors";
+import * as defaults from "styles/defaults";
 
 const Container = styled.div`
-	display: block;
-	background: ${colors.bgDark};
-	padding: 200px 5%;
-`
+  display: block;
+  background: ${colors.bgDark};
+  padding: 200px 5%;
+`;
 
 const Title = styled.h2`
-	font-size: 8vmin;
-	font-weight: 400;
-	margin: 0;
-	text-align: center;	
-`
+  font-size: 8vmin;
+  font-weight: 400;
+  margin: 0;
+  text-align: center;
+`;
 
 const SubTitle = styled.p`
-	font-size: 3vmin;
-	font-weight: 500;
-	text-align: center;
-	margin: 0;
-	opacity: .6;
-	color: ${colors.textColor};
-`
+  font-size: 3vmin;
+  font-weight: 500;
+  text-align: center;
+  margin: 0;
+  opacity: 0.6;
+  color: ${colors.textColor};
+`;
 
 const EmailContainer = styled.div`
-	position: relative;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	margin: 50px;
-	button {
-		position: absolute;
-		right: 20%;
-		top: 0;
-	}
-	@media screen and (max-width: 1000px) {
-		flex-direction: column;
-		input {
-			margin-bottom: 50px;	
-		}
-		button {
-			position: static;
-		}
-	}
-	@media screen and (max-width: 768px) {
-		margin: 50px 0 0 0;
-	}
-`
+  width: 50%;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 50px auto;
+  button {
+    position: absolute;
+    right: -10%;
+    top: 0;
+    bottom: 0;
+  }
+  @media screen and (max-width: 1000px) {
+    flex-direction: column;
+    input {
+      margin-bottom: 50px;
+    }
+    button {
+      position: static;
+    }
+  }
+  @media screen and (max-width: 768px) {
+    margin: 50px 0 0 0;
+  }
+`;
 
 const EmailInput = styled.input`
-	width: 50%;
-	border: none;
-	padding: 33px 0 33px 15px;
-	font-size: 1em;
-	box-shadow: 0 5px 15px lightgrey;
-	border: 2px solid white;
-	transition: border .3s ease;
-	&:focus, &:hover {
-		outline: none;
-		outline-color: ${colors.btnPrimary};
-		border: 2px solid ${colors.btnPrimary};		
-	}
+  width: 100%;
+  border: none;
+  padding: 33px 0 33px 15px;
+  font-size: 1em;
+  box-shadow: 0 5px 15px lightgrey;
+  border: 3px solid white;
+  transition: border 0.3s ease;
+  &:focus,
+  &:hover {
+    outline: none;
+    outline-color: ${colors.btnPrimary};
+	border-color: ${colors.btnPrimary};
+    /* border: 4px solid ${colors.btnPrimary}; */
+  }
 
-	&:focus {
-		${(props) => !props.$isEmpty && css`
-			&:valid {
-				border: 2px solid green;
-			}	
-		`}
+  &:focus {
+    ${(props) =>
+      !props.$isEmpty &&
+      css`
+        &:valid {
+          border-color: green;
+          /* border: 2px solid green; */
+        }
+      `}
 
-		&:invalid {
-			border: 2px solid red;
-		}
-	}
+    &:invalid {
+		border-color: red;
+      /* border: 2px solid red; */
+    }
+  }
 
-	@media screen and (max-width: 768px) {
-		width: 80%;
-		padding: 20px 0 20px 15px;
-	}
-`
+  @media screen and (max-width: 768px) {
+    width: 80%;
+    padding: 20px 0 20px 15px;
+  }
+`;
 
 const SubmitBtn = styled.button`
-	font-family: 'poppins';
-	font-size: 2em;
-	cursor: pointer;
-	text-decoration: none;
-	color: white;
-	background: ${colors.btnPrimary};
-	padding: ${defaults.buttonPadding};
-	border-radius: 50px;
-	transition: background .3s ease;
-	border: none;
-	@media screen and (max-width: 768px) {
-		font-size: 1.5em;
-	}
-	@media screen and (max-width:  480px) {
-		font-size: 1em;
-	}
-	&:hover {
-		background: ${colors.btnHover};
-	}
+  font-family: "poppins";
+  font-size: 1.3em;
+  cursor: pointer;
+  text-decoration: none;
+  color: white;
+  background: ${colors.btnPrimary};
+  padding: ${defaults.buttonPadding};
+  border-radius: 50px;
+  transition: background 0.3s ease;
+  border: none;
+  @media screen and (max-width: 768px) {
+    font-size: 1.5em;
+  }
+  @media screen and (max-width: 480px) {
+    font-size: 1em;
+  }
+  &:hover {
+    background: ${colors.btnHover};
+  }
 
-	${(props) => props.$isDisabled && css`
-		background: ${colors.btnDisabled};
-	`}
+  ${(props) =>
+    props.$isDisabled &&
+    css`
+      background: ${colors.btnDisabled};
+    `}
 
-	&:disabled {
-		cursor: not-allowed;
+  &:disabled {
+    cursor: not-allowed;
 
-		&:hover {
-			background: ${colors.btnDisabled};
-		}
-	}
-`
+    &:hover {
+      background: ${colors.btnDisabled};
+    }
+  }
+`;
 
 const Contact = () => {
-	const [contactEmail, setContactEmail] = useState("");
-	const [emailIsValid, setEmailIsValid] = useState(false);
+  const [contactEmail, setContactEmail] = useState("");
+  const [emailIsValid, setEmailIsValid] = useState(false);
 
-	const onChangeEmailInput = (event) => {
-		setContactEmail(event.target.value);
+  const onChangeEmailInput = (event) => {
+    setContactEmail(event.target.value);
 
-		if (event.target.value) {
-			if (event.target.value.length > 0) {
-				if (event.target.validity.valid) {
-					setEmailIsValid(true);
-				} else {
-					setEmailIsValid(false);
-				}
-			} else {
-				setEmailIsValid(false);
-			}
-		} else {
-			setEmailIsValid(false);
-		}
-	}
+    if (event.target.value) {
+      if (event.target.value.length > 0) {
+        if (event.target.validity.valid) {
+          setEmailIsValid(true);
+        } else {
+          setEmailIsValid(false);
+        }
+      } else {
+        setEmailIsValid(false);
+      }
+    } else {
+      setEmailIsValid(false);
+    }
+  };
 
-	return (
-		<Container id="contact">
-			<Title>Let&apos;s work together</Title>
-			<SubTitle>I will reach out to you asap</SubTitle>
-			<form name="contact" method="POST" data-netlify="true">
-				<input type="hidden" name="form-name" value="contact" />
-				<EmailContainer>
-					<EmailInput onChange={onChangeEmailInput} value={contactEmail} $isEmpty={!contactEmail} placeholder="Email" type="email" name="email" />
-					<SubmitBtn $isDisabled={!emailIsValid} disabled={!contactEmail} type="submit">Contact me</SubmitBtn>
-				</EmailContainer>
-			</form>
-		</Container>
-		)
-}
+  return (
+    <Container id="contact">
+      <Title>Let&apos;s work together</Title>
+      <SubTitle>I will reach out to you asap</SubTitle>
+      <form name="contact" method="POST" data-netlify="true">
+        <input type="hidden" name="form-name" value="contact" />
+        <EmailContainer>
+          <EmailInput
+            onChange={onChangeEmailInput}
+            value={contactEmail}
+            $isEmpty={!contactEmail}
+            placeholder="Email"
+            type="email"
+            name="email"
+          />
+          <SubmitBtn
+            $isDisabled={!emailIsValid}
+            disabled={!contactEmail}
+            type="submit"
+          >
+            Contact me
+          </SubmitBtn>
+        </EmailContainer>
+      </form>
+    </Container>
+  );
+};
 
 export default Contact;
